32c176a24ee58d385eaf9ebca586821bd706a87c
chihuahuaStudio <82788392+chihuahuaStudio@users.noreply.github.com>
2023-01-17 15:48:10 -0500

    Canvas mods
    Seperate canvas for text, interactables and updated gameobjects

--- Assets/Scripts/Gameplay/DeadBox.cs
+++ Assets/Scripts/Gameplay/DeadBox.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DeadBox : MonoBehaviour
@@ -8,9 +5,9 @@ public class DeadBox : MonoBehaviour
     
     private void OnTriggerEnter(Collider other)
     {
-        gameObject.SetActive(false);
+        other.gameObject.SetActive(false);
     
-        if (!gameObject.CompareTag("Bad"))
+        if (!other.gameObject.CompareTag("Bad"))
         {
             GameManager.Singleton.GameOver();
             LivesManager.UpdateLivesLeft();
